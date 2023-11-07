b484eacca9ceae74071e525ad97d530b678aa837
Pedro K. L <pedro.karasudani@hotmail.com>
2023-11-06 21:58:24 -0300

    Implementacao basica

--- mudi/src/main/java/br/com/alura/mvc/mudi/WebSegurityConfig.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/WebSegurityConfig.java
@@ -16,8 +16,11 @@ public class WebSegurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+			.and()
+				.httpBasic();
 	}
 	
 	@Bean
